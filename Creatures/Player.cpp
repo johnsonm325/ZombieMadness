bff33ecb9e24fa15e6cd0576d32bfd8928132bba
Jeremy Einhorn <jeremyeinhorn93@gmail.com>
2019-02-26 21:03:47 -0500

    all colors loaded in

--- Creatures/Player.cpp
+++ Creatures/Player.cpp
@@ -1,5 +1,11 @@
 #include "Player.h"
 #define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
 #define RESET "\x1B[0m"
 using std::cout;
 using std::endl;
