5920f4aef59cde2f7619fd5255964fcbb35729bc
Michael Johnson <micjohns@noob.usersys.redhat.com>
2019-02-26 19:07:15 -0500

    Rewrite room descriptions

--- Rooms/GymnasiumFloor1.cpp
+++ Rooms/GymnasiumFloor1.cpp
@@ -4,15 +4,15 @@ GymnasiumFloor1::GymnasiumFloor1() : Space("Gymnasium First Floor")
 {
 	this->zombie = new Zombie(false);
 	basketball = new Item();
-	string description = "# A single basketball is seen in the middle of the gym floor. It looks like it's filled with air.";
+	string description = "# A single basketball is seen in the middle of the gym floor. It\n# looks like it's filled with air.";
 	string name = "basketball";
 	basketball->setDummyItem(description, name);
-	string text = "# You grab the basketball and shoot a few hoops. You don't make a single shoot. 'Good thing no one is here to see that.'";
+	string text = "# You grab the basketball and shoot a few hoops. You don't make a single\n# shot. 'Good thing no one is here to see that.'";
 	basketball->setAction(text, Use);
 	roomInventory->addItem(basketball);
 
 	bleachers = new Item();
-	description = "# The bleachers are pulled out of the wall and there are some scattered bodies littered on them.";
+	description = "# The bleachers are pulled out of the wall and there are some scattered\n# bodies littered on them.";
 	name = "bleachers";
 	bleachers->setDummyItem(description, name);
 	roomInventory->addItem(bleachers);
@@ -20,7 +20,9 @@ GymnasiumFloor1::GymnasiumFloor1() : Space("Gymnasium First Floor")
 
 GymnasiumFloor1::~GymnasiumFloor1()
 {
-	
+	delete zombie;
+	delete basketball;
+	delete bleachers;
 }
 
 void GymnasiumFloor1::printIntro(){
@@ -28,19 +30,21 @@ void GymnasiumFloor1::printIntro(){
 	if (firstTry == true)
 	{
 		if(deadZombies == true) {
-			cout << "# Dead zombies litter the floor. Great thing you decided to cut those ropes or else you'd have a tough fight" << endl;
-			cout << "# on your hands. Basketball doesn't seem like such of an important event when you're having to fight your way" << endl;
-			cout << "# out of this place to stay alive." << endl;
+			cout << "# Dead zombies litter the floor. Great thing you decided to cut those ropes or" << endl;
+			cout << "# else you'd have a tough fight on your hands. Basketball doesn't seem like such" << endl;
+			cout << "# of an important event when you're having to fight your way out of this place" << endl;
+			cout << "# to stay alive." << endl;
 			cout << "#" << endl;
 		}
 
 		else {
-			cout << "# You are surrounded by zombies. Packs of them notice your presence and slowly begin to shuffle their way in" << endl;
-			cout << "# your direction." << endl;
+			cout << "# You are surrounded by zombies. Packs of them notice your presence and slowly" << endl;
+			cout << "# begin to shuffle their way in your direction." << endl;
 			cout << "#" << endl;
 		}
 
-		cout << "# Basketball goals are mounted against the west and east walls, and bleachers are pulled out along the south wall." << endl;
+		cout << "# Basketball goals are mounted against the west and east walls, and bleachers" << endl;
+		cout << "# are pulled out along the south wall. There is a basketball on the floor." << endl;
 		cout << "#" << endl;
 		cout << "# What do you do now?" << endl;
 		cout << "#" << endl;
@@ -51,22 +55,16 @@ void GymnasiumFloor1::printExitDesc()
 {
 	if (firstTry == true)
 	{
-		if (deadZombies == true)
-		{
-			cout << "# 'Wow, thank goodness I was able to take out all these zombies at once," << endl;
-			cout << "# no way could I take them all on!'" << endl;
-		}
-
-		else
-		{
-			cout << "# There are seriously a ton of these zombies here, how can you ever fight them?" << endl;
-			cout << "# ..." << endl;
-			cout << "# Or maybe you don't?" << endl;
-		}
+		cout << "# 'Wow, thank goodness I was able to take out all these zombies at once," << endl;
+		cout << "# no way could I take them all on!'" << endl;
+		cout << "#" << endl;
+		cout << "# There are three ways out of the Gym. I can either head up to the second" << endl;
+		cout << "# floor or head out one of two doors." << endl;
 	}
 
-	cout << "# The cafeteria is super close, you remember that from how many times you went to grab food" << endl;
-	cout << "# during gym class.  The football field is easily accessible from here." << endl;
+	cout << "# The cafeteria is super close, you remember that from how many times you" << endl;
+	cout << "# went to grab food during gym class.  The football field is easily accessible" << endl;
+	cout << "# from here." << endl;
 	cout << "#" << endl;
 }
 
--- Rooms/MensBathroom.cpp
+++ Rooms/MensBathroom.cpp
@@ -3,7 +3,7 @@
 MensBathroom::MensBathroom() : Space("Men's Bathroom")
 {
 	toilet = new Item();
-	string description = "# The toilet is dirty. Looks like it hasn't been cleaned in weeks. As you stare at the toilet, you notice a hole behind the toilet.";
+	string description = "# The toilet is dirty. Looks like it hasn't been cleaned in weeks. As you\n# stare at the toilet, you notice a hole behind the toilet.";
 	string name = "toilet";
 	toilet->setDummyItem(description, name);
 	roomInventory->addItem(toilet);
@@ -17,19 +17,22 @@ void MensBathroom::printIntro(){
 	// Prints the first time the room is visited
 	if (firstTry == true)
 	{
-		cout << "# You jump awake, realizing that you've been asleep on the toilet for quit a while. I guess staying up so late" << endl;
-		cout << "# studying has taken its toll on you. 'How long have I been asleep?' you ask yourself. 'Have I missed all of my" << endl;
-		cout << "# classes?' Something doesn't seem right to you. The emergency lights are on and you don't hear any noises" << endl;
-		cout << "# outside of the door. 'Maybe everyone left and someone shut off the lights for the night." << endl;
+		cout << "# You jump awake, realizing that you've been asleep on the toilet for quit a while." << endl;
+		cout << "# I guess staying up so late studying has taken its toll on you. 'How long have I" << endl;
+		cout << "# been asleep?' you ask yourself. 'Have I missed all of my classes?' Something" << endl;
+		cout << "# doesn't seem right to you. The emergency lights are on and you don't hear any" << endl;
+		cout << "# noises outside of the door. 'Maybe everyone left and someone shut off the lights" << endl;
+		cout << "# for the night." << endl;
 		cout << "#" << endl;
-		cout << "# You pull off a few squares of tissue paper and wipe your butt, now crusted up from the hours your feces spent" << endl;
-		cout << "# drying while you were asleep. It hurts to move your legs since they have fallen asleep from sitting too long." << endl;
-		cout << "# You finish wiping, stand up, pull up your pants and examine the room around you. 'Well, I guess I need to" << endl;
-		cout << "# get out of here.' You step out of the stall, and into the bathroom." << endl;
+		cout << "# You pull off a few squares of tissue paper and wipe. It hurts to move your legs" << endl;
+		cout << "# since they have fallen asleep from sitting too long. You finish wiping, stand" << endl;
+		cout << "# up, pull up your pants and examine the room around you. 'Well, I guess I need" << endl;
+		cout << "# to get out of here.' You step out of the stall, and into the bathroom." << endl;
 		cout << "#" << endl;
 	}
 
-	cout << "# Sinks stand against the west wall, and urinals and toilets line the east wall. Nothing much going on in here." << endl;
+	cout << "# Sinks stand against the west wall, and urinals and toilets line the east wall. Nothing" << endl;
+	cout << "# much going on in here." << endl;
 	cout << "#" << endl;
 	cout << "# What do you do now?" << endl;
 	cout << "#" << endl;
@@ -40,9 +43,9 @@ void MensBathroom::printExitDesc()
 	if (firstTry == true)
 	{
 		firstTime();
-		cout << "# 'Well, here goes nothing...'  Your journe of terror begins now!" << endl;
-		cout << "# You really are unsure of what the rest of the school looks like right now," << endl;
-		cout << "# only one way to find out..." << endl;
+		cout << "# 'Well, here goes nothing... Let's hope the front doors aren't locked." << endl;
+		cout << "# You really are unsure if anyone is left in the school right now, or if" << endl;
+		cout << "# you'll get in trouble. Looks like there's only one way out." << endl;
 		cout << "#" << endl;
 	}
 
@@ -68,7 +71,7 @@ void MensBathroom::inspectToilet()
 	holeVisible = true;
 
 	hole = new Item();
-	string description = "# There is a small hole behind the toilet. It looks like you could probably fit through it.";
+	string description = "# There is a small hole behind the toilet. It looks like you could\n# probably fit through it.";
 	string name = "hole";
 	hole->setDummyItem(description, name);
 	roomInventory->addItem(hole);
